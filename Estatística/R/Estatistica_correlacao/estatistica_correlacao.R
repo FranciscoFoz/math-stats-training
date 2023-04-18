5a20f2304211da57a4aa5a412f4ad178e2b2e002
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-04-18 06:31:33 -0300

    Atualizando correlação em R

--- Estatística/R/Estatistica_correlacao/estatistica_correlacao.R
+++ Estatística/R/Estatistica_correlacao/estatistica_correlacao.R
@@ -3,33 +3,28 @@ library(tidyverse)
 library(lubridate)
 library(ggplot2)
 
-
-datas <- seq(from = as.Date("2022-01-01"), to = as.Date("2022-12-31"), by = "days")
-
-#Gerando os valores aleatórios para o índice de precipitacao
-precipitacao <- as.integer(rnorm(365, mean = 50, sd = 10))
-
+#Criando dataset
+#datas <- seq(from = as.Date("2022-01-01"), to = as.Date("2022-12-31"), by = "days")
+#precipitacao <- as.integer(rnorm(365, mean = 50, sd = 10))
 #Gerando as visitas à biblioteca digital com base no índice de precipitacao
-visitas <- precipitacao * 5 + rnorm(365, mean = 0, sd = 22) + 500
-visitas <- as.integer(visitas)
+#visitas <- as.integer(precipitacao * 5 + rnorm(365, mean = 0, sd = 22) + 500)
+#df <- data.frame(data = datas, visitas_biblioteca_digital= visitas, precipitacao = precipitacao)
+#head(df)
 
-#Criando um dataframe a partir do dicionário
-df <- data.frame(data = datas, visitas_biblioteca_digital= visitas, precipitacao = precipitacao)
-
-#Exibindo as primeiras linhas do dataframe
+df <- read.csv('/home/franciscofoz/Documents/GitHub/math-stats-training/Datasets/visitas_biblioteca_digital_precipitacao/correlacao_visitas_biblioteca_precipitacao.csv',
+               sep=';')
 head(df)
 
 #Gráfico de dispersão
 ggplot(data = df, aes(x = visitas_biblioteca_digital, y = precipitacao)) +
       geom_point(color = "darkblue", size = 3) +
       labs(title = "Dispersão de Visitas a Biblioteca Digital X Índice de Precipitação", 
-            x = "Visitas a biblioteca digital", y = "Índice de Precipitação")
+            x = "Visitas a biblioteca digital", y = "Índice de Precipitação") + 
+      geom_smooth(method = "lm",formula = y ~ x)
 
 #Covariância
 cov(df[,2:3])
 var(df[,3])
-
-
 
 #Correlação apartir da covariância
 cov_visitas_precipitacao <- cov(df[,2:3])['visitas_biblioteca_digital','precipitacao']
