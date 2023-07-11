aa2e5e3ea3d0aa2291e450a8525831f9764075a3
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-07-11 07:15:27 -0300

    Init R distribuição binomial

---
+++ Estatística/R/Estatistica_Probabilidade/Distribuicao_Binomial.R
@@ -0,0 +1,14 @@
+# Distribuição Binomial
+
+
+## Espaço amostral da mega-sena
+combinacoes_mega_sena <- choose(60,6)
+combinacoes_mega_sena
+
+combinacoes_lotofacil <- choose(25,15)
+combinacoes_lotofacil
+
+probabilidade_acerto_lotofacil <- 1000/ combinacoes_lotofacil
+probabilidade_acerto_lotofacil
+
+
